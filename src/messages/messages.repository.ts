67aeaf8fded35c74cab3ff150becf6affb360a9d
Julia Frederico <magalhaesjulia@id.uff.br>
2025-07-26 20:38:46 -0300

    feat: implement MessagesRepository with findAll method for DynamoDB

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,1 +1,34 @@
-// TODO: Implementar repositório de mensagens com funções para manipulação de mensagens no DynamoDB
+import { DynamoDBClient, ScanCommand } from '@aws-sdk/client-dynamodb';
+import { Injectable } from '@nestjs/common';
+import { DynamoDBService } from '../config/database.config';
+import { Message } from './entities/message.entity';
+
+@Injectable()
+export class MessagesRepository {
+  private readonly tableName = 'messages';
+  private readonly client: DynamoDBClient;
+
+  constructor(private readonly dynamoService: DynamoDBService) {
+    this.client = this.dynamoService.getClient();
+  }
+
+  async findAll(): Promise<Message[]> {
+    const result: Message[] = [];
+
+    const command = new ScanCommand({
+      TableName: this.tableName,
+    });
+
+    const response = await this.client.send(command);
+
+    if (response.Items) {
+      response.Items.forEach((item) => {
+        console.log('Item:', item);
+        const message = Message.newInstanceFromDynamoDB(item);
+        result.push(message);
+      });
+    }
+
+    return result;
+  }
+}
