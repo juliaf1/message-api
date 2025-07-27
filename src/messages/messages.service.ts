12c1b0d7d1c9a5485dee7cae173d36f4e7ca0119
Julia Frederico <magalhaesjulia@id.uff.br>
2025-07-27 15:01:11 -0300

    refactor: message service to use messages dynamo repository

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,56 +1,19 @@
-import {
-  Injectable,
-  NotFoundException,
-  BadRequestException,
-} from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { CreateMessageDto } from './dto/create-message.dto';
 import {
   MessageStatusDto,
   UpdateMessageStatusDto,
 } from './dto/update-message-status.dto';
-import { v4 as uuidv4 } from 'uuid';
+import { Message } from './entities/message.entity';
 import { MessagesRepository } from './messages.repository';
 
 @Injectable()
 export class MessagesService {
   constructor(private readonly repository: MessagesRepository) {}
 
-  private messages = [
-    {
-      id: '90f81cec-c915-4940-b648-bd263053722c',
-      content: 'Hello!',
-      senderId: 'user-uuid-1',
-      status: 'SENT',
-    },
-    {
-      id: 'uuid-2',
-      content: 'How are you?',
-      senderId: 'user-uuid-2',
-      status: 'SENT',
-    },
-    {
-      id: 'uuid-3',
-      content: 'Good morning.',
-      senderId: 'user-uuid-3',
-      status: 'DELIVERED',
-    },
-    {
-      id: 'uuid-4',
-      content: 'See you soon.',
-      senderId: 'user-uuid-4',
-      status: 'DELIVERED',
-    },
-    {
-      id: 'uuid-5',
-      content: 'Thanks!',
-      senderId: 'user-uuid-5',
-      status: 'SEEN',
-    },
-  ];
-
   findAll(senderId?: string) {
     console.log(senderId);
-    return this.repository.findAll();
+    return this.repository.findBySenderId(senderId, new Date(), new Date());
   }
 
   findOne(id: string) {
@@ -59,20 +22,8 @@ export class MessagesService {
   }
 
   create(createMessageDto: CreateMessageDto) {
-    const id: string = uuidv4();
-
-    const newMessage: {
-      id: string;
-      content: string;
-      senderId: string;
-      status: 'SENT';
-    } = {
-      id: id,
-      ...createMessageDto,
-      status: 'SENT',
-    };
-    this.messages.push(newMessage);
-    return newMessage;
+    const message: Message = Message.newInstanceFromDTO(createMessageDto);
+    return this.repository.upsertOne(message);
   }
 
   async updateStatus(
@@ -94,15 +45,7 @@ export class MessagesService {
       );
     }
 
-    // TODO: Salvar no "banco"
-    this.messages = this.messages.map((message) => {
-      if (message.id === id) {
-        return { ...message, ...updateMessageStatusDto };
-      }
-      return message;
-    });
-
-    // TODO: Retornar DTO de mensagem atualizada
-    return this.findOne(id);
+    message.status = updateMessageStatusDto.status;
+    return await this.repository.upsertOne(message);
   }
 }
