1ec925816a227ea845a052f5612f1edc18aab520
Julia Frederico <magalhaesjulia@id.uff.br>
2025-07-27 15:00:12 -0300

    feat: add recipientPhoneNumber property to CreateMessageDto for message creation

--- src/messages/dto/create-message.dto.ts
+++ src/messages/dto/create-message.dto.ts
@@ -19,4 +19,12 @@ export class CreateMessageDto extends PartialType(MessageStatusDto) {
   @IsString()
   @IsNotEmpty()
   senderId: string;
+
+  @ApiProperty({
+    description: 'ID of the recipient',
+    example: 'c1d2e3f4-5a6b-7c8d-9e0f-1a2b3c4d5e6f',
+  })
+  @IsString()
+  @IsNotEmpty()
+  recipientPhoneNumber: string;
 }
