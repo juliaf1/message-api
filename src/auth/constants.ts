79dc38ad5c6bb41e8e9357436dfff273e7a20f8b
Julia Frederico <magalhaesjulia@id.uff.br>
2025-07-28 18:46:33 -0300

    chore: add JWT environment variables to docker-compose and update jwtConstants to remove defaults

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,5 +1,5 @@
 export const jwtConstants = {
-  secret: process.env.JWT_SECRET || '',
-  user_pass: process.env.JWT_USER_PASS || '',
-  system_pass: process.env.JWT_SYSTEM_PASS || '',
+  secret: process.env.JWT_SECRET,
+  user_pass: process.env.JWT_USER_PASS,
+  system_pass: process.env.JWT_SYSTEM_PASS,
 };
