6bd6ea07a288243560ad45e28bbda8e0069058d7
Julia Frederico <magalhaesjulia@id.uff.br>
2025-07-27 21:11:53 -0300

    feat: adds authentication flow with system user login and bcrypt password comparison

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,63 @@
-
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
-    private jwtService: JwtService
+    private jwtService: JwtService,
   ) {}
 
   async signIn(
     username: string,
     pass: string,
   ): Promise<{ access_token: string }> {
-    const user = await this.usersService.findOne({ externalId: username });
-    // if (user?.password !== pass) {
-    //   throw new UnauthorizedException();
-    // }
-    console.log(pass);
-    const payload = { sub: user.userId, userExternalId: user.externalId };
+    let token: string;
+
+    if (username === 'SYSTEM') {
+      token = await this.loginSystemUser(pass);
+    } else {
+      token = await this.loginUser(username, pass);
+    }
+
     return {
-      access_token: await this.jwtService.signAsync(payload),
+      access_token: token,
     };
+  }
+
+  private async loginSystemUser(password: string): Promise<string> {
+    const isMatch: boolean = await bcrypt.compare(
+      password,
+      jwtConstants.system_pass,
+    );
+    if (isMatch) {
+      const payload = { sub: 'SYSTEM', userExternalId: 'SYSTEM' };
+      return await this.jwtService.signAsync(payload);
+    }
+
+    throw new UnauthorizedException('Invalid credentials');
+  }
+
+  private async loginUser(username: string, password: string): Promise<string> {
+    let user = await this.usersService.findOne({ externalId: username });
+    if (!user) {
+      user = await this.usersService.create({
+        externalId: username,
+      });
+    }
+
+    const isMatch: boolean = await bcrypt.compare(
+      password,
+      jwtConstants.user_pass,
+    );
+    if (isMatch) {
+      const payload = { sub: user.userId, userExternalId: user.externalId };
+      return await this.jwtService.signAsync(payload);
+    }
+
+    throw new UnauthorizedException('Invalid credentials');
   }
 }
